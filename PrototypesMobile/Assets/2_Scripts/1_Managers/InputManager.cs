2ee0d0c92e79901dd0b0a0731d7d60860ea67aa5
louhichio <louhichio@hotmail.com>
2015-10-15 17:01:24 +0200

    Stable Version
    Debug Input Manager
    Gesture recognition
    Zoom Max, Middle and Min Values On Camera

--- PrototypesMobile/Assets/2_Scripts/1_Managers/InputManager.cs
+++ PrototypesMobile/Assets/2_Scripts/1_Managers/InputManager.cs
@@ -7,14 +7,8 @@
 	{
 		private bool isStop = false;
 		[SerializeField]
-		private bool isPinching = false;
-		[SerializeField]
-		private bool isTap = false;
-		[SerializeField]
-		private bool isSwipe = false;
-		[SerializeField]
-		private bool isDrag = false;
-
+		private bool isFingerClear = false;
+		
 		#region Events
 		void OnEnable()
 		{
@@ -39,6 +33,12 @@
 
 		void Update()
 		{
+			if(Input.touchCount > 1 )
+				isFingerClear = false;
+			
+			if(!isFingerClear && Input.touchCount == 0)
+				isFingerClear = true;
+
 			if (!isStop && Input.GetKeyDown(KeyCode.Escape)) 
 				Application.Quit();
 
@@ -47,31 +47,21 @@
 				if(!isStop && 
 				   TurnManager.Instance.turnState == TurnState.PlayerTurn &&
 				   Player.Instance.moveState == MoveState.None && 
-				   !isPinching)
+				   isFingerClear)
 				{
 					Tile t = RetrieveTouchTilePosition();
 					if(t != null)
 						Player.Instance.TouchOnDestinationTile(t);
 				}
 			}
-//
-//			print ("isPinching: " + isPinching + 
-//			       "  isTap: " + isTap +
-//			       "  isSwipe:  " + isSwipe +
-//			       "  isDrag:  " + isDrag + 
-//			       "  TouchCount:  " + Input.touchCount);
-			isTap = false;
 		}		
 		
 		void OnTap(TapGesture gesture) 
 		{
-			if(!isTap)
-				isTap = true;
-
 			if(!isStop && 
 			   TurnManager.Instance.turnState == TurnState.PlayerTurn &&
 			   Player.Instance.moveState == MoveState.None && 
-			   !isPinching)
+			   isFingerClear && Input.touchCount == 1)
 			{
 				Tile t = RetrieveTouchTilePosition();
 
@@ -84,74 +74,66 @@
 						TurnManager.Instance.StartCoroutine("PlayerMoved");
 				}
 			}
-//			if(gesture.State == GestureRecognitionState.Ended)
-//				isTap = false;
 		}
 		
 		void OnSwipe(SwipeGesture gesture) 
 		{
-			if(!isSwipe)
-				isSwipe = true;
-
 			if(!isStop && 
 			   TurnManager.Instance.turnState == TurnState.PlayerTurn && 
 			   Player.Instance.moveState == MoveState.None && 
-			   !isPinching)
+			   isFingerClear && Input.touchCount == 1)
 			{
+				print ("OnSwipe");
 				Player.Instance.TravelTo(
 					MapManager.Instance.GetSwipeTilePosition(Player.Instance.tile_current, gesture.Direction));
 			}
-			if(gesture.State == GestureRecognitionState.Ended)
-				isSwipe = false;
 		}
 
 		void OnPinch(PinchGesture gesture) 
 		{
-			if(!isPinching)
-			{
-				isPinching = true;
-				Player.Instance.DisableDestinationTile();
-			}
-
+			Player.Instance.DisableDestinationTile();
 			if(!isStop && 
 			   TurnManager.Instance.turnState == TurnState.PlayerTurn)
 				CameraManager.Instance.OnPinch(gesture);
-
-			if(gesture.State == GestureRecognitionState.Ended)
-				isPinching = false;
 		}
 
 		void OnDrag( DragGesture gesture ) 
 		{
-			if(!isDrag)
-				isDrag = true;
-
 			if(!isStop && 
 			   TurnManager.Instance.turnState == TurnState.PlayerTurn &&
 			   Player.Instance.moveState == MoveState.None &&
-			   !isPinching)
+			   isFingerClear && Input.touchCount == 1)
 			{
 				Tile t = RetrieveTouchTilePosition();
-				if(t != null)
-					Player.Instance.TouchOnDestinationTile(t);
 
+				if(t != null)
+					Player.Instance.TouchOnDestinationTile(t);		
+				else					
+					Player.Instance.DisableDestinationTile();
+				
 				if(gesture.State == GestureRecognitionState.Ended)
-				{					
-					if(t != null && t != Player.Instance.tile_current)					
-						Player.Instance.TravelTo(t);					
-					else
+				{		
+					if(t != null)
 					{
-						Player.Instance.TravelFinished();
-						if(t == Player.Instance.tile_current)
+						if(t != Player.Instance.tile_current)
+						{
+							Player.Instance.TouchOnDestinationTile(t);
+							Player.Instance.TravelTo(t);
+						}
+						else
+						{	
+							/////// End Player Turn
+							Player.Instance.TravelFinished();
 							TurnManager.Instance.StartCoroutine("PlayerMoved");
+						}
 					}
+					else					
+						Player.Instance.DisableDestinationTile();
 				}
 			}
-
-			if(gesture.State == GestureRecognitionState.Ended)
-				isDrag = false;
+			
 		}
-
+		
 		private Tile RetrieveTouchTilePosition()
 		{
 			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
